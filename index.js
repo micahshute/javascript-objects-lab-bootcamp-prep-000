0b1601f57423706c749e44250ad678305ec81ec7
Micah Shute <micah.shute@gmail.com>
2017-12-13 02:31:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value){
+  newObj = {};
+  Object.assign(newObj,object,{key:value})
+  return newObj
   
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object.key = value
+  return object
+}
+
+fuction deleteFromObjectByKey(object, key){
+  
+}
+
